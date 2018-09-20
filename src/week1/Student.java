9a564bf25f73489317040c95702cfc5f8209aac4
duong2808 <43176802+duong2808@users.noreply.github.com>
2018-09-21 00:04:05 +0700

    Revert "update"
    This reverts commit c569b751cc84a6e91666a2e57ab42d0415a94263.

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,53 +1,16 @@
-﻿package week1;
+package week1;
 
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
-	private String name, id, group, email;
 
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
      * Constructor 1
      */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setGroup(String group) {
-		this.group = group;
-	}
-
-	public String getGroup() {
-		return group;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getEmail() {
-		return email;
-	}
     Student() {
         // TODO:
-	name = "Student";
-	id = "000";
-	group = "INT22041";
-	email = "uet@gmail.com";
     }
 
     /**
@@ -58,10 +21,6 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
-	name = n;
-        id = sid;
-        email = em;
-        group = "INT22041";
     }
 
     /**
@@ -70,18 +29,10 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
-	name = s.name;
-	id = s.id;
-	group = s.group;
-	email = s.email;
     }
 
     String getInfo() {
         // TODO:
-	System.out.println("name:" + name);
-	System.out.println("id:" + id);
-	System.out.println("group:" + group);
-	System.out.println("email:" + email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }
