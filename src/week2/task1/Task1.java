b7a57c819b8a887475a71628cd85a6820d9fcee7
duong2808 <43176802+duong2808@users.noreply.github.com>
2018-09-27 00:01:03 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,28 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if(a<0)a=-a;
+	if(b<0)b=-b;
+	if(a==0 && b!=0)return b;
+	if(a!=0 && b==0)return a;
+	else{
+	while(a!=b){
+		if(a>b)a=a-b;
+		else b=b-a;
+		}
+	return a;
+	}
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+         (n==0 || n==1) return n;
+        else return Fibonaci(n-1)+Fibonaci(n-2);
+    }
+    public static void main(String[] args) {
+        int a=32, b=-6;
+        System.out.println(gcd(a,b));
+        int n=8;
+        System.out.println(Fibonaci(n));
     }
 }
