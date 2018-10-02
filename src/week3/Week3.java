66095943e42a434c09cd1a294bc06a695c85b1a4
duong2808 <43176802+duong2808@users.noreply.github.com>
2018-10-03 00:08:03 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -3,13 +3,25 @@ package week3;
 public class Week3 {
 
     public static int max(int m, int n) {
+        if(m>n)return m;
+        else if(n>m){
+            return n;
+            }
+        else
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
         return -1;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int a=array.length;
+        int min=array[0];
+        for(int i=0;i<a;i++){
+            if(min>array[i]){
+               min=array[i];
+                }
+            }
+        return min;
+        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử
     }
 
     /**
@@ -19,7 +31,13 @@ public class Week3 {
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
     public static String calculateBMI(double weight, double height) {
+        double BMI=weight/(height*height);
+        if(BMI<18.5)return("Thiếu cân");
+        if(BMI>=18.5 && BMI<22.9)return("Bình thường");
+        if(BMI>=23 && BMI<24.99)return("Thừa cân");
+        else return ("Béo phì");
+        }
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
-    }
+
+    
 }
