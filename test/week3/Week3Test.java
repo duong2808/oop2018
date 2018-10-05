314672eb3775275f213cd5d53bef52f3c7b24552
duong2808 <43176802+duong2808@users.noreply.github.com>
2018-10-05 09:49:33 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -5,9 +5,7 @@ import org.junit.Before;
 
 import org.junit.Test;
 
-
-
-
+import static junit.framework.TestCase.assertEquals;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
@@ -21,30 +19,30 @@ public class Week3Test {
     int[] array5={99,103,63,39,108,202};
 @Test    
 public void testmax(){
-     Assert.assertEquals(2,week3.Week3.max(2,1));
-     Assert.assertEquals(-1,week3.Week3.max(1,1));
-     Assert.assertEquals(100,week3.Week3.max(100,80));
-     Assert.assertEquals(28,week3.Week3.max(8,28));
-     Assert.assertEquals(36,week3.Week3.max(32,36));
+     assertEquals(2,Week3.max(2,1));
+     assertEquals(-1,Week3.max(1,1));
+     assertEquals(100,Week3.max(100,80));
+     assertEquals(28,Week3.max(8,28));
+     assertEquals(36,Week3.max(32,36));
     }
 @Test
 public void testminOfArray1(){
-    min=week3.Week3.testminOfArray(array1);
+    min=Week3.testminOfArray(array1);
     assertEquals(min,1);
 }
 @Test
 public void testminOfArray2(){
-    min=week3.Week3.testminOfArray(array2);
+    min=Week3.testminOfArray(array2);
     assertEquals(min,13);
 }
 @Test
 public void testminOfArray3(){
-    min=week3.Week3.testminOfArray(array3);
+    min=Week3.testminOfArray(array3);
     assertEquals(min,0);
 }
 @Test
 public void testminOfArray4(){
-    min=week3.Week3.testminOfArray(array4);
+    min=Week3.testminOfArray(array4);
     assertEquals(min,1);
 }
 public void testminOfArray5(){
@@ -53,11 +51,11 @@ public void testminOfArray5(){
 }
 @Test
 public void testcalculateBMI(){
-    Assert.assertEquals("Thiếu cân",week3.Week3.calculateBMI(46,1.60));
-    Assert.assertEquals("Bình thường",week3.Week3.calculateBMI(62,1.70));
-    Assert.assertEquals("Thừa cân",week3.Week3.calculateBMI(67,1.67));
-    Assert.assertEquals("Béo phì",week3.Week3.calculateBMI(70,1.65));
-    Assert.assertEquals("Thiếu cân",week3.Week3.calculateBMI(40.5,1.5));
+    assertEquals("Thiếu cân",Week3.calculateBMI(46,1.60));
+    assertEquals("Bình thường",Week3.calculateBMI(62,1.70));
+    assertEquals("Thừa cân",Week3.calculateBMI(67,1.67));
+    assertEquals("Béo phì",Week3.calculateBMI(70,1.65));
+    assertEquals("Thiếu cân",Week3.calculateBMI(40.5,1.5));
     
     }
     // TODO: Viết 5 testcase cho phương thức max()
