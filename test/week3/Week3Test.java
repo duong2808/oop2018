2ac14f245132bef81604bc4ae13c7cd2fa1534ca
duong2808 <43176802+duong2808@users.noreply.github.com>
2018-10-05 13:23:46 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,15 +1,8 @@
 package week3;
 import week3.Week3.*;
-
-import org.junit.Before;
-
 import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
 import org.junit.Assert;
-import static org.junit.Assert.*;
 
-import static week3.Week3.max;
 public class Week3Test {
     int min=0;
     int[] array1={3,2,1};
@@ -18,44 +11,68 @@ public class Week3Test {
     int[] array4={70,43,67,2,7,19,30,1,25,55};
     int[] array5={99,103,63,39,108,202};
 @Test    
-public void testmax(){
-     assertEquals(2,Week3.max(2,1));
-     assertEquals(-1,Week3.max(1,1));
-     assertEquals(100,Week3.max(100,80));
-     assertEquals(28,Week3.max(8,28));
-     assertEquals(36,Week3.max(32,36));
+public void testmax1(){
+     Assert.assertEquals(2,Week3.max(2,1));
+}
+ @Test    
+public void testmax2(){
+    Assert.assertEquals(-1,Week3.max(1,1));
+}
+    @Test    
+public void testmax3(){
+     Assert.assertEquals(100,Week3.max(100,80));
+}
+    @Test    
+public void testmax4(){
+     Assert.assertEquals(28,Week3.max(8,28));
+}
+    @Test    
+public void testmax5(){
+     Assert.assertEquals(36,Week3.max(32,36));
     }
 @Test
 public void testminOfArray1(){
     min=Week3.testminOfArray(array1);
-    assertEquals(min,1);
+      Assert.assertEquals(min,1);
 }
 @Test
 public void testminOfArray2(){
     min=Week3.testminOfArray(array2);
-    assertEquals(min,13);
+     Assert.assertEquals(min,13);
 }
 @Test
 public void testminOfArray3(){
     min=Week3.testminOfArray(array3);
-    assertEquals(min,0);
+     Assert.assertEquals(min,0);
 }
 @Test
 public void testminOfArray4(){
     min=Week3.testminOfArray(array4);
-    assertEquals(min,1);
+     Assert.assertEquals(min,1);
 }
 @Test
 public void testminOfArray5(){
     min=week3.Week3.testminOfArray(array5);
-    assertEquals(min,39);
+     Assert.assertEquals(min,39);
 }
 @Test
 public void testcalculateBMI(){
     assertEquals("Thiếu cân",Week3.calculateBMI(46,1.60));
+}
+    @Test
+public void testcalculateBMI(){
     assertEquals("Bình thường",Week3.calculateBMI(62,1.70));
+}
+    @Test
+public void testcalculateBMI(){
     assertEquals("Thừa cân",Week3.calculateBMI(67,1.67));
+}
+    @Test
+public void testcalculateBMI(){
     assertEquals("Béo phì",Week3.calculateBMI(70,1.65));
+}
+    @Test
+public void testcalculateBMI(){
     assertEquals("Thiếu cân",Week3.calculateBMI(40.5,1.5));
     
     }
